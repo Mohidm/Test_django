d86c77d517a2cc726cabea4aa1bb27e848652b5a
Mohid <mohidm94@gmail.com>
2020-08-10 22:01:01 +0530

    init

--- kelloggsPringles/kelloggsPringles/settings.py
+++ kelloggsPringles/kelloggsPringles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q=kb3yg*etob*=js(89h#^68cxw*vp2eu45*dh&#f4hpedxl_n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
